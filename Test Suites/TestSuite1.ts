2c4887b1cb399c77f9b247545cad006bd2b2ce03
Mayowa Oyesetan <moyesetan@netcomafrica.com>
2020-03-18 22:51:58 +0100

    Kim test cases

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>706c55b9-8fd9-4428-9ac0-a76807f811e3</testSuiteGuid>
+</TestSuiteEntity>
